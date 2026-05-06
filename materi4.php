18e2ccc8a4d43abc4650899eb23136b26fde04ff
RadityaAhmad26 <radityaahmad018@gmail.com>
2026-05-06 08:53:58 +0800

    pertemuan 6

--- materi4.php
+++ materi4.php
@@ -24,3 +24,38 @@ if (isset($_POST['kirim'])) {
 }
 
 ?>
+
+<table boarder="1" cellpading="10" cellspacing="0">
+    <tr>
+        <th>Username</th>
+        <th>Password</th>
+        <th>Nama</th>
+        <th>Email</th>
+        <th>Aksi</th>
+    </tr>
+
+
+<?php
+$query = "SELECT * FROM user";
+$result = mysqli_query($koneksi, $query);
+while ($row = mysqli_fetch_assoc($result)) {
+    echo "<tr>";
+    echo "<td>" . $row['username'] . "</td>";
+    echo "<td>" . $row['password'] . "</td>";
+    echo "<td>" . $row['nama'] . "</td>";
+    echo "<td>" . $row['email'] . "</td>";
+    echo "<td><a href='materi4.php?id=" . $row['id'] . "'>Edit | <a href='materi4.php?id=" . $row['id'] . "'>Hapus</a></td>";
+    echo "</tr>";
+}
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $query = "DELETE FROM user WHERE id = $id";
+    if (mysqli_query($koneksi, $query)) {
+        echo "Data berhasil dihapus";
+    } else {
+        echo "Data gagal dihapus";
+    }
+}
+?>
+</table>
